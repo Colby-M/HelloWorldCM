e1b3ae2ac7309bcabdccc8f49ec55e1a7ad47111
Colby McDonald <67612969+Colby-M@users.noreply.github.com>
2020-10-29 13:45:21 -0400

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (Colby McDonald)
+ * @version (29/10/20)
+ */
+public class HelloWorld
+{
+    /**
+     * Constructor for objects of class HelloWorld
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World, this is Colby!");
+    }
+}
